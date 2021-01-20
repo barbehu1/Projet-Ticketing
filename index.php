b205ebfd51d376807b5ba55a46a09f3912cfeff2
barbehu1 <43534736+barbehu1@users.noreply.github.com>
2021-01-20 16:07:11 +0100

    Avancement
    Avencement

--- index.php
+++ index.php
@@ -5,8 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion | Plateforme de Ticketing</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    
+<body class="body-connexion text-center">
+    <div class="container container-connexion col-3 bg-light p-5 mb-3 font-weight-bold">
+        <h1 class="titre-connexion mb-4">Plateforme de ticketing</h1>
+        <form action="" class="text-left">
+            <label for="mail" class="mb-0 label-connexion">Adresse mail</label>
+            <br>
+            <input type="mail" class="input-connexion rounded-pill">
+            <br>
+            <label for="mdp" class="mb-0 label-connexion">Mot de passe</label>
+            <br>
+            <input type="password" class="input-connexion rounded-pill">
+            <br>
+            <div class="centre-bouton-connexion mb-3 mt-5">
+                <button type="submit" class="bouton-connexion rounded-pill text-light font-weight-bold text-center">Connexion</button>
+            </div>
+        </form>
+        <a href="" class="font-weight-normal mdp-link"><u>Mot de passe oublié ?</u></a>
+    </div>
+    <a class="text-light mt-2 compte-link" href=""><u>Pas encore de compte ?<br>Demandez un accès.</u></a>
 </body>
 </html>
