833b0862eaa073595a83739d447d76cbae867743
barbehu1 <43534736+barbehu1@users.noreply.github.com>
2021-01-21 09:36:04 +0100

    Create menu-gauche.php

---
+++ menu-gauche.php
@@ -0,0 +1,17 @@
+<div class="col-2 menu-gauche-ticket">
+    <div class="user bg-light">
+        <img src="https://picsum.photos/id/237/71/71" class="rounded-circle float-left">
+        <p class="user-name font-weight-bold">Loïc Guéniau</p>
+        <p class="user-type">Utilisateur</p>
+    </div>
+    <div class="menu">
+        <ul class="font-weight-bold">
+            <li class="selected bg-light rounded-pill"><i class="far fa-file-alt"></i> Tickets</li>
+            <li><i class="far fa-clipboard"></i> Projets</li>
+            <li><i class="fas fa-cog"></i> Paramètres</li>
+        </ul>
+    </div>
+    <div class="disconnection">
+        <p class="font-weight-bold mx-auto"><i class="fas fa-power-off"></i> Déconnexion</p>
+    </div>
+</div>
